840af1c57d64c1bfe206676ade967b31bac2978f
wangjiawei601996 <wangjiawei601996@pwrd.com>
2021-06-02 19:40:50 +0800

    local

--- oooccc/main.cpp
+++ oooccc/main.cpp
@@ -5,6 +5,24 @@
 //  Created by wangjiawei on 2021/6/1.
 //
 
+/*
+ 
+ 函数存储信息 运行时栈动态分配
+ 递归函数调用资深，系统如何区分局部变量？
+ 
+ 活动记录activation record(栈框架stack frame) [运行时栈]
+ 1. 所有局部变量内容
+ 2. 函数的参数值
+ 3. 表明在调用函数的何处重新开始的返回地址
+ 
+ 函数在执行状态下，隶属于该函数的活动记录一直存在，
+ 是函数的私有信息池，存储了程序正确执行并正确返回的调用它的函数所需要的所有信息
+ 
+ 寿命短，主函数寿命最长
+ 
+ 
+ */
+
 #include <iostream>
 #include "IntSLList.hpp"
 #include "GenDLList.hpp"
